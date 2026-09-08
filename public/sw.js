f8a93e6d2f5dbc511696b33dd22412a2ace7ceb1
RhaonyFerraz <112454732+RhaonyFerraz@users.noreply.github.com>
2026-09-08 18:04:43 -0300

    style: destaca separacao dos quadros com bordas coloridas tematicas, sombras e fundo contrastante

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Suporte offline, cache inteligente e alta disponibilidade
  */
 
-const CACHE_NAME = 'clima-rmc-cache-v4';
+const CACHE_NAME = 'clima-rmc-cache-v5';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
