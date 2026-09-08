a8328d3334219d3b45d82eba051a9e71b732cb88
RhaonyFerraz <112454732+RhaonyFerraz@users.noreply.github.com>
2026-09-08 18:11:38 -0300

    style(pwa): altera texto do botao para 'Instalar' e remove emoji/icone

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Suporte offline, cache inteligente e alta disponibilidade
  */
 
-const CACHE_NAME = 'clima-rmc-cache-v5';
+const CACHE_NAME = 'clima-rmc-cache-v6';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
